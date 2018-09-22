93fe038b6e1952358b6db8e901a73870ce3a6934
Sreeram Ramakrishnan <sriram3rhyme@gmail.com>
2018-09-22 18:30:32 -0400

    Define Arithmetic Operations

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,13 +11,43 @@ import UIKit
 class ViewController: UIViewController {
     
     var valOnScreen:Double = 0
-
+    var firstval:Double = 0
+    var mathFunction  = false
+    var arithop = 0
+    
     @IBOutlet weak var result: UILabel!
     
     
     @IBAction func digits(_ sender: UIButton) {
         result.text! += String(sender.tag-1)
         valOnScreen = Double(result.text!)!
+    }
+    
+    @IBAction func functions(_ sender: UIButton) {
+        
+        if result.text != "" && sender.tag != 11 && sender.tag != 16
+        {
+            firstval = Double(result.text!)!
+            if sender.tag == 12
+            {
+                result.text = "+"
+            }
+            if sender.tag == 13
+            {
+                result.text = "-"
+            }
+            if sender.tag == 14
+            {
+                result.text = "*"
+            }
+            if sender.tag == 15
+            {
+                result.text = "/"
+            }
+            
+            arithop = sender.tag
+            mathFunction = true
+        }
     }
     
     
